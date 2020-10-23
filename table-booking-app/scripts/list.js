228332393671e2911dccef2b96d5259ecede622b
Germaine Khoo <Germaine@example.com>
2020-10-23 21:40:29 +0800

    remove default row in table

--- table-booking-app/scripts/list.js
+++ table-booking-app/scripts/list.js
@@ -27,7 +27,7 @@ function GetBookings(){
         row.insertCell (2).innerHTML = gEmail;
         row.insertCell (3).innerHTML = gPax;
         row.insertCell (4).innerHTML = gRemarks;
-        row.insertCell (5).innerHTML = "<button id='" + buttonId + "'>Delete</button><br/>";
+        row.insertCell (5).innerHTML = "<button id='" + buttonId + "' class='btn btn-danger'>Delete</button><br/>";
     
        bookingIds.push(buttonId);
       }
@@ -41,6 +41,3 @@ function GetBookings(){
       }
     }); 
     }
-    document.getElementById("booking-list").addEventListener("click", function(){
-         GetBookings()                                                 
-        })
